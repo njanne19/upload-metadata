877cd8eca953a9d672e3fa08e8b606ed123f25fc
Nick Janne <nickjanne7@gmail.com>
2017-06-30 08:33:28 -0400

    Trying to fix Heroku issues

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var multer = require('multer');
-var upload = multer({ dest: '../public/uploads'});
+var upload = multer({ dest: '../tmp/uploads'});
 
 
 
